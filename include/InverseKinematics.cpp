11067a4d4dae68216233c22b845c948c9c851d7c
hannbusann <1617126946@qq.com>
2018-10-30 00:17:57 +0800

    first commit

---
+++ include/InverseKinematics.cpp
@@ -0,0 +1,4 @@
+//
+// Created by zjudancer on 18-10-29.
+//
+
---
+++ include/InverseKinematics.h
@@ -0,0 +1,26 @@
+//
+// Created by zjudancer on 18-10-29.
+// E-mail: zjufanwu@zju.edu.cn
+//
+
+#ifndef PROJECT_INVERSEKINEMATICS_H
+#define PROJECT_INVERSEKINEMATICS_H
+
+
+#include <Eigen/Dense>
+#include "../Polynomial/Polynomial.hpp"
+
+namespace dmotion {
+
+    class InvKin {
+    private:
+
+    public:
+        static const upper_leg_length;
+        static const lower_leg_length;
+        static const
+    };
+}
+
+
+#endif //PROJECT_INVERSEKINEMATICS_H

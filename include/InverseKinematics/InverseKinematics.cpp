c4e48bc3d3fcf68533a7fa2e30387bc7109b9edb
hannbusann <1617126946@qq.com>
2018-10-31 17:35:18 +0800

    general commit

--- include/InverseKinematics/InverseKinematics.cpp
+++ include/InverseKinematics/InverseKinematics.cpp
@@ -7,17 +7,41 @@
 #include "dmotion_math.hpp"
 
 namespace dmotion {
-    std::vector<double> & InvKin::LegInvKin(std::vector<double> &foot_pose, bool isRight) {
+
+    InvKin::InvKin(bool isRight) {
+        isRight_ = isRight;
+        hip_yaw_ = 0;
+        hip_roll_ = 0;
+        hip_pitch_ = 0;
+        knee_pitch_ = 0;
+        ankle_pitch_ = 0;
+        ankle_roll_ = 0;
+    }
+
+    std::vector<double> &InvKin::LegInvKin(std::vector<double> &foot_pose, bool isRight) {
         // 下面三个坐标是脚踝点相对于髋关节点的坐标
-        double ankle_x_to_hip = foot_pose[0] + ankle_from_ground*(std::sin(Deg2Rad(foot_pose[3]))*std::sin(Deg2Rad(foot_pose[5]))
-                + std::std::cos(Deg2Rad(foot_pose[3]))*std::std::cos(Deg2Rad(foot_pose[5]))*std::sin(Deg2Rad(foot_pose[4])))
-                        - hip_x_from_origin;
-        double ankle_y_to_hip = foot_pose[1] - ankle_from_ground*(std::cos(Deg2Rad(foot_pose[5]))*std::sin(Deg2Rad(foot_pose[3]))
-                - std::cos(Deg2Rad(foot_pose[3]))*std::sin(Deg2Rad(foot_pose[4]))*std::sin(Deg2Rad(foot_pose[5])))
-                        - (isRight?(-half_hip_width):half_hip_width);
-        double ankle_z_to_hip = foot_pose[2] + ankle_from_ground*cos(pp)*cos(rr)
-                - hip_z_from_origin;
+        double ankle_x_to_hip = foot_pose[0] + ankle_from_ground *
+                                               (std::sin(Deg2Rad(foot_pose[3])) * std::sin(Deg2Rad(foot_pose[5])) +
+                                                std::std::cos(Deg2Rad(foot_pose[3])) *
+                                                std::std::cos(Deg2Rad(foot_pose[5])) *
+                                                std::sin(Deg2Rad(foot_pose[4]))) - hip_x_from_origin;
+        double ankle_y_to_hip = foot_pose[1] - ankle_from_ground *
+                                               (std::cos(Deg2Rad(foot_pose[5])) * std::sin(Deg2Rad(foot_pose[3])) -
+                                                std::cos(Deg2Rad(foot_pose[3])) * std::sin(Deg2Rad(foot_pose[4])) *
+                                                std::sin(Deg2Rad(foot_pose[5]))) -
+                                (isRight ? (-half_hip_width) : half_hip_width);
+        double ankle_z_to_hip = foot_pose[2] + ankle_from_ground * cos(pp) * cos(rr) - hip_z_from_origin;
+        double ankle_norm = std::sqrt(
+                ankle_x_to_hip * ankle_x_to_hip + ankle_y_to_hip * ankle_y_to_hip + ankle_z_to_hip * ankle_z_to_hip);
+        hip_pitch_ = CosineTheorem(upper_leg_length, ankle_norm, lower_leg_length);
+        knee_pitch_ = 180 - CosineTheorem(upper_leg_length,lower_leg_length,ankle_norm);
 
+    }
 
+    double InvKin::CosineTheorem(const double &edge_1,
+                                 const double &edge_2,
+                                 const double &edge_opppsite) {
+        double cosine = (edge_1 * edge_1 + edge_2 * edge_2 - edge_opppsite * edge_opppsite) / (2 * edge_1 * edge_2);
+        return std::acos(cosine);
     }
 }
--- include/InverseKinematics/InverseKinematics.h
+++ include/InverseKinematics/InverseKinematics.h
@@ -13,8 +13,17 @@
 namespace dmotion {
 
     class InvKin {
-    private:
 
+    private:
+        // 这些是单个腿上的关节值
+        double hip_yaw_;
+        double hip_roll_;
+        double hip_pitch_;
+        double knee_pitch_;
+        double ankle_pitch_;
+        double ankle_roll_;
+        // 是左腿还是右腿
+        bool isRight_;
     public:
         // 静态常成员变量便于调用,这些参数比较固定,机器人装好后一般绝对不会变,故没有写在参数文件中
         // 参数的单位是cm
@@ -25,14 +34,21 @@ namespace dmotion {
         static const double hip_x_from_origin = 0;    //髋关节点相对于身体中心原点的x方向坐标
         static const double hip_z_from_origin = -8.0; //髋关节点相对于身体中心原点的z
 
+        // 构造函数简单的构造一下就可以
+        InvKin(bool isRight);
+
 
         // 逆运动学,以身体中心为原点,前x左y上z的方向建立坐标系,输入左/右脚的位置(x,y,z)和姿态(r,p,y)
         // 为了方便理解和调参,这里的RPY变换根据以下描述进行变换
         // 起始状态脚坐标系B和此处的世界坐标系A坐标轴方向重合,先绕Z_a转yaw角,再绕Y_b转pitch角,最后绕X_b转roll角.
         // 输出从上到下的共6个舵机角度值(角度值).
-        std::vector<double> & LegInvKin(std::vector<double> &foot_pose, bool isRight);
+        static std::vector<double> &LegInvKin(std::vector<double> &foot_pose,
+                                              bool isRight);
 
-
+        // 余弦定理的逆定理,用三角形三边求角度值
+        inline double CosineTheorem(const double &edge_1,
+                                    const double &edge_2,
+                                    const double &edge_opppsite)
 
 
     };

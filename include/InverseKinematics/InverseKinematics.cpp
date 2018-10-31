9bb4b7ed7588d3d08a9fde5c4cb9aaea36cad8db
hannbusann <1617126946@qq.com>
2018-10-31 16:12:54 +0800

    add matlab and complete ankle analysis

--- include/InverseKinematics/InverseKinematics.cpp
+++ include/InverseKinematics/InverseKinematics.cpp
@@ -2,3 +2,22 @@
 // Created by zjudancer on 18-10-29.
 //
 
+#include "InverseKinematics.h"
+#include <cmath>
+#include "dmotion_math.hpp"
+
+namespace dmotion {
+    std::vector<double> & InvKin::LegInvKin(std::vector<double> &foot_pose, bool isRight) {
+        // 下面三个坐标是脚踝点相对于髋关节点的坐标
+        double ankle_x_to_hip = foot_pose[0] + ankle_from_ground*(std::sin(Deg2Rad(foot_pose[3]))*std::sin(Deg2Rad(foot_pose[5]))
+                + std::std::cos(Deg2Rad(foot_pose[3]))*std::std::cos(Deg2Rad(foot_pose[5]))*std::sin(Deg2Rad(foot_pose[4])))
+                        - hip_x_from_origin;
+        double ankle_y_to_hip = foot_pose[1] - ankle_from_ground*(std::cos(Deg2Rad(foot_pose[5]))*std::sin(Deg2Rad(foot_pose[3]))
+                - std::cos(Deg2Rad(foot_pose[3]))*std::sin(Deg2Rad(foot_pose[4]))*std::sin(Deg2Rad(foot_pose[5])))
+                        - (isRight?(-half_hip_width):half_hip_width);
+        double ankle_z_to_hip = foot_pose[2] + ankle_from_ground*cos(pp)*cos(rr)
+                - hip_z_from_origin;
+
+
+    }
+}
--- include/InverseKinematics/InverseKinematics.h
+++ include/InverseKinematics/InverseKinematics.h
@@ -16,19 +16,25 @@ namespace dmotion {
     private:
 
     public:
-        static const double upper_leg_length = 12.0;  //大腿的长度,静态常成员变量便于调用,这个参数比较固定,一般绝对不会变,故没有写在参数文件中
+        // 静态常成员变量便于调用,这些参数比较固定,机器人装好后一般绝对不会变,故没有写在参数文件中
+        // 参数的单位是cm
+        static const double upper_leg_length = 12.0;  //大腿的长度
         static const double lower_leg_length = 12.0;  //小腿的长度
         static const double ankle_from_ground = 3.5;  //脚踝距离地面的高度
-        static const double half_hip_width = 4.5;     //两髋关节距离的一半
-        static const double hip_x_from_origin = 0;    //原点到髋关节x方向上的距离
+        static const double half_hip_width = 4.5;     //两髋关节点距离的一半,相当于髋关节点相对于身体中心原点的y方向坐标
+        static const double hip_x_from_origin = 0;    //髋关节点相对于身体中心原点的x方向坐标
+        static const double hip_z_from_origin = -8.0; //髋关节点相对于身体中心原点的z
 
 
-        // 逆运动学,以身体为原点,前x左y上z的方向建立坐标系,输入左/右脚的位置(x,y,z)和姿态(r,p,y)
+        // 逆运动学,以身体中心为原点,前x左y上z的方向建立坐标系,输入左/右脚的位置(x,y,z)和姿态(r,p,y)
+        // 为了方便理解和调参,这里的RPY变换根据以下描述进行变换
+        // 起始状态脚坐标系B和此处的世界坐标系A坐标轴方向重合,先绕Z_a转yaw角,再绕Y_b转pitch角,最后绕X_b转roll角.
         // 输出从上到下的共6个舵机角度值(角度值).
-        std::vector<double> & LegInvKin()
+        std::vector<double> & LegInvKin(std::vector<double> &foot_pose, bool isRight);
 
 
-        //以上参数的单位都是cm
+
+
     };
 }
 
